7b73393cdd20a66b1f8183067ed224ac66e2cdb3
Mohd Sahil <beingsahil99@gmail.com>
2022-02-22 14:26:07 +0530

    reformat assignment table

--- Testing at Items level/Data Wrangling (Items level).sql
+++ Testing at Items level/Data Wrangling (Items level).sql
@@ -17,17 +17,80 @@ FROM
  
  
 /*
-Reformat the final_assignments_qa to look like the final_assignments table, 
-filling in any missing values with a placeholder of the appropriate data type.
+Reformating the final_assignments_qa table to look like the final_assignments table
 */
 
-SELECT 
-  item_id,
-  test_a       AS test_assignment, 
-  'test_a'     AS test_number, 
-  '2020-01-01' AS test_start_date
-FROM 
-  dsv1069.final_assignments_qa
+SELECT item_id,
+       test_a AS test_assignment,
+       (CASE
+            WHEN test_a IS NOT NULL then 'test_a'
+            ELSE NULL
+        END) AS test_number,
+       (CASE
+            WHEN test_a IS NOT NULL then '2013-01-05 00:00:00'
+            ELSE NULL
+        END) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION
+SELECT item_id,
+       test_b AS test_assignment,
+       (CASE
+            WHEN test_b IS NOT NULL then 'test_b'
+            ELSE NULL
+        END) AS test_number,
+       (CASE
+            WHEN test_b IS NOT NULL then '2013-01-05 00:00:00'
+            ELSE NULL
+        END) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION
+SELECT item_id,
+       test_c AS test_assignment,
+       (CASE
+            WHEN test_c IS NOT NULL then 'test_c'
+            ELSE NULL
+        END) AS test_number,
+       (CASE
+            WHEN test_c IS NOT NULL then '2013-01-05 00:00:00'
+            ELSE NULL
+        END) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION
+SELECT item_id,
+       test_d AS test_assignment,
+       (CASE
+            WHEN test_d IS NOT NULL then 'test_d'
+            ELSE NULL
+        END) AS test_number,
+       (CASE
+            WHEN test_d IS NOT NULL then '2013-01-05 00:00:00'
+            ELSE NULL
+        END) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION
+SELECT item_id,
+       test_e AS test_assignment,
+       (CASE
+            WHEN test_e IS NOT NULL then 'test_e'
+            ELSE NULL
+        END) AS test_number,
+       (CASE
+            WHEN test_e IS NOT NULL then '2013-01-05 00:00:00'
+            ELSE NULL
+        END) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION
+SELECT item_id,
+       test_f AS test_assignment,
+       (CASE
+            WHEN test_f IS NOT NULL then 'test_f'
+            ELSE NULL
+        END) AS test_number,
+       (CASE
+            WHEN test_f IS NOT NULL then '2013-01-05 00:00:00'
+            ELSE NULL
+        END) AS test_start_date
+FROM dsv1069.final_assignments_qa
 
 
 
